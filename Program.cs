00c0c3cd02c1563dab418fa1c5cc50d9680a280c
Sherlock <playyala@yahoo.com>
2025-12-12 21:16:04 +0200

    update cors error

--- Program.cs
+++ Program.cs
@@ -142,8 +142,7 @@ builder.Services.AddCors(options =>
     {
         builder.AllowAnyOrigin()
                .AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowCredentials();
+               .AllowAnyMethod();
     });
 });
 
